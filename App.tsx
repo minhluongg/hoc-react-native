1ff24aee0d815456a3a49aa70c3c88ce9fdb9c19
minhluongg <luongtd@sfun.vn>
2025-03-21 23:45:47 +0700

    Video 30

--- App.tsx
+++ App.tsx
@@ -1,31 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   const [name, setName] = useState<string>("Minh Luong");
-  const [age, setAge] = useState<number>(0);
-  const [person, setPerson] = useState<{
-    name: string;
-    age: number;
-  }>({ name: "Minh Luong", age: 0 });
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Xin chào thế giới!</Text>
+      <TextInput
+        style={styles.input}
+        // autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Enter your name"
+        onChangeText={(val) => setName(val)}
+        // defaultValue="You can type in me ds"
+      ></TextInput>
       <View>
         <Text>{name}</Text>
       </View>
       <View>
-        <Text>{age}</Text>
+        <Button
+          title="Add new"
+          onPress={() => Alert.alert("Simple Button pressed")}
+        />
       </View>
-      <View>
-        <Text>{JSON.stringify(person)}</Text>
-      </View>
-      <TextInput
-        style={styles.input}
-        defaultValue="You can type in me ds"
-      ></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -39,6 +38,8 @@ const styles = StyleSheet.create({
     width: "100%",
     borderRadius: 5,
     padding: 10,
+    marginBottom: 16,
+    marginTop: 16,
   },
   container: {
     flex: 1,
