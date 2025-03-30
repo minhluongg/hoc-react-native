34263e91f0c391ba0c22eb3938222438e5b392c7
minhluongg <luongtd@sfun.vn>
2025-03-30 16:32:15 +0700

    video 48

--- App.tsx
+++ App.tsx
@@ -1,59 +1,62 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 import {
-  Keyboard,
-  StyleSheet,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
-import InputTodo from "./components/todo/Input.Todo";
-import ListTodo from "./components/todo/List.Todo";
-import FlexBox from "./components/todo/FlexBox";
+  NavigationContainer,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
-  const [todoList, setTodoList] = useState<ITodo[]>([]);
-
-  function randomInteger(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  const addTodo = (title: string) => {
-    const newTodo: ITodo = {
-      id: randomInteger(1, 1000000),
-      title,
-    };
-    setTodoList([...todoList, newTodo]);
-  };
-
-  const deleteTodo = (id: number) => {
-    const newTodoList = todoList.filter((todo) => todo.id !== id);
-    setTodoList(newTodoList);
-  };
+function HomeScreen() {
+  const navigation: any = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <FlexBox></FlexBox>
-    </View>
-    // <TouchableWithoutFeedback
-    //   onPress={() => {
-    //     Keyboard.dismiss();
-    //   }}
-    // >
-    //   <View style={styles.container}>
-    //     <InputTodo addTodo={addTodo}></InputTodo>
-    //     <ListTodo todoList={todoList} deleteTodo={deleteTodo} />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
 
-    //     <StatusBar style="auto" />
-    //   </View>
-    // </TouchableWithoutFeedback>
+      <View style={{ marginBottom: 20 }}>
+        <Button
+          title="Go to Details 1"
+          onPress={() => navigation.navigate("Details", { itemId: 1 })}
+        />
+      </View>
+      <View style={{ marginBottom: 20 }}>
+        <Button
+          title="Go to Details 2"
+          onPress={() => navigation.navigate("Details", { itemId: 2 })}
+        />
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 50,
-    paddingHorizontal: 16,
-  },
-});
+function DetailsScreen() {
+  const route: any = useRoute();
+  const navigation: any = useNavigation();
+  const { itemId } = route.params;
+  console.log(route);
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen {itemId}</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Details"
+          options={({ route }: { route: any }) => ({
+            title: `Details ${route.params?.itemId}`,
+          })}
+          component={DetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
